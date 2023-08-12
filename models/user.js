03523be9ae32d16ca128f37b5b3ce18a697ead9d
Akshat Agarwal <76041127+Akshat-z@users.noreply.github.com>
2023-08-12 15:38:00 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,24 +1,24 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const schema = mongoose.Schema({
- name :{
-  type:String,
-  require:[true],
- },
- email:{
-  type:String,
-  require:[true],
-  unique:[true], // no dublicate data
- },
- password:{
-  type:String,
-  require:[true],
-  select:[false], // means data will store buy not visible in database and we can not directly access it.
- },
- createdAt:{
-  type:Date,
-  default:Date.now,
- }
-})
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true, // no dublicate data
+  },
+  password: {
+    required: true,
+    type: String,
+    select: false, // means data will store buy not visible in database and we can not directly access it.
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-export const user = mongoose.model("user",schema);
+export const User = mongoose.model("User", schema);
